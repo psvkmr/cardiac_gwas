a41141b4064709e0a245f29f3638503ca58ebe42
penstrep <prasanth_sivakumar@hotmail.co.uk>
2022-01-28 01:50:11 +0000

    create ramsey sample tables

--- gwas_run.sh
+++ gwas_run.sh
@@ -1,5 +1,10 @@
 srun -p brc --mem 5G --pty /bin/bash
 
+# links
+#https://github.com/alanmichaelpittman100/GWAS-Imputation-Protocol-/blob/master/HRC-1000G-check-bim-v4.2_AP_MODIFIED.pl
+#https://genome.sph.umich.edu/wiki/Regions_of_high_linkage_disequilibrium_(LD)
+
+
 #Load Plink  module
 module load apps/plink2/2.0.0a2
 
@@ -14,3 +19,10 @@ for i in seq `1 22`;
 do
   plink2 --bgen ${data_path}/ukb_imp_chr${i}_v3_MAF1_INFO4.bgen ref-first  \
   --sample ${out_dir}/ukb22828_c1_b0_v3_s487253.sample
+
+
+# activate SAIGE
+# conda activate RSAIGE
+#  FLAGPATH=`which python | sed 's|/bin/python$||'`
+#  export LDFLAGS="-L${FLAGPATH}/lib"
+#  export CPPFLAGS="-I${FLAGPATH}/include"
