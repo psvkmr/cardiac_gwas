ab39e6e3a37c29d2e4a358a206f56e60e73ac829
penstrep <prasanth_sivakumar@hotmail.co.uk>
2022-01-29 17:16:11 +0000

    plink stages

--- gwas_run.sh
+++ gwas_run.sh
@@ -35,9 +35,9 @@ mkdir -p $out_dir
 for i in `seq 1 2`;
 do
 #  plink2 --bgen ${raw_data}/ukb_imp_chr${i}_v3_MAF1_INFO4.bgen ref-first  \
-#  --sample ${raw_data}/ukb22828_c${i}_b0_v3_s487253.sample 
-  plink2 --pfile ${out_dir}/chr${i} --keep-fam ${raw_data}/IDs_final.txt --make-pgen --out ${out_dir}/qc1_chr${i}_keepfam 
-#  plink2 --pfile ${out_dir}/qc1_chr${i}_keepfam --indep-pairwise 50 5 0.2 --out LDprune_chr${i} 
+#  --sample ${raw_data}/ukb22828_c${i}_b0_v3_s487253.sample --out ${out_dir}/chr${i}
+  plink2 --pfile ${out_dir}/chr${i} --keep-fam ${raw_data}/IDs_final.txt --make-pgen --out ${out_dir}/qc1_chr${i}_keepfam
+#  plink2 --pfile ${out_dir}/qc1_chr${i}_keepfam --indep-pairwise 50 5 0.2 --out LDprune_chr${i}
 #  plink2 --pfile ${out_dir}/qc1_chr${i}_keepfam --extract ${out_dir}/LDprune_chr${i}.prune.in --make-pgen --out ${out_dir}/qc2_chr${i}_prune
 done
 
