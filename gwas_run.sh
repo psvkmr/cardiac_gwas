2c58a2cdf5071e687a913a9abe7bafbb1abe1b59
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-02-01 10:47:47 +0000

    plink qc steps 1-3

--- gwas_run.sh
+++ gwas_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --partition=brc
-#SBATCH --time=04:00:00
-#SBATCH --mem=30G
-#SBATCH --ntasks=1
+#SBATCH --partition=brc,shared
+#SBATCH --time=02:00:00
+#SBATCH --mem=42G
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=8
 #SBATCH --job-name=gwas
 #SBATCH --verbose
@@ -32,10 +32,13 @@ out_dir=/scratch/users/k2142172/outputs/cardiac_gwas/gwas_run
 
 mkdir -p $out_dir
 
-for i in seq `1 2`;
+for i in `seq 1 2`;
 do
-  plink2 --bgen ${raw_data}/ukb_imp_chr${i}_v3_MAF1_INFO4.bgen ref-first  \
-  --sample ${raw_data}/ukb22828_c${i}_b0_v3_s487253.sample
+#  plink2 --bgen ${raw_data}/ukb_imp_chr${i}_v3_MAF1_INFO4.bgen ref-first  \
+#  --sample ${raw_data}/ukb22828_c${i}_b0_v3_s487253.sample 
+  plink2 --pfile ${out_dir}/chr${i} --keep-fam ${raw_data}/IDs_final.txt --make-pgen --out ${out_dir}/qc1_chr${i}_keepfam 
+#  plink2 --pfile ${out_dir}/qc1_chr${i}_keepfam --indep-pairwise 50 5 0.2 --out LDprune_chr${i} 
+#  plink2 --pfile ${out_dir}/qc1_chr${i}_keepfam --extract ${out_dir}/LDprune_chr${i}.prune.in --make-pgen --out ${out_dir}/qc2_chr${i}_prune
 done
 
 # activate SAIGE
