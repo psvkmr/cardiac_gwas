d829c1b7c3667ea03645be075910dd35bbf29b80
penstrep <prasanth_sivakumar@hotmail.co.uk>
2022-02-25 08:03:17 +0000

    qc samples

--- qc_samples.R
+++ qc_samples.R
@@ -2,6 +2,7 @@
 
 setwd('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas')
 
+# plink-calculated inbreeding coefficient and expected vs observed heterozygosity rates
 het.files <- lapply(X = list.files(pattern = '*.het',
                                    path = 'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/', 
                                    full.names = T), 
@@ -16,17 +17,43 @@ het.names <- lapply(X = list.files(pattern = '*.het',
 
 names(het.files) <- het.names                    
 
-hetChecker <- function(x){
-#  x$het_rate <- ((x$`O.HOM.` - x$`E.HOM.`) / x$`O.HOM`)
-#  y <- subset(x, (x$het_rate < (mean(x$het_rate) - 3*sd(x$het_rate)) | x$het_rate > (mean(x$het_rate) + 3*sd(x$het_rate))))
-  y <- subset(x, (F < -0.2 | F > 0.2))
-  return(y$IID)
-}
-het.fail <- lapply(het.files, hetChecker)
-het.fail.tbl <- sort(table(unlist(het.fail)), decreasing = T)
+# hetChecker <- function(x){
+# #  x$het_rate <- ((x$`O.HOM.` - x$`E.HOM.`) / x$`O.HOM`)
+# #  y <- subset(x, (x$het_rate < (mean(x$het_rate) - 3*sd(x$het_rate)) | x$het_rate > (mean(x$het_rate) + 3*sd(x$het_rate))))
+#   y <- subset(x, (F < -0.2 | F > 0.2))
+#   return(y$IID)
+# }
+# het.fail <- lapply(het.files, hetChecker)
+# het.fail.tbl <- sort(table(unlist(het.fail)), decreasing = T)
 
-# not removing any samples for now, possible threshold for F +- 0.2
-#lapply(het.files, function(x) x[x$IID == '1543065', 'F'])
+# get matrices of observed and expected hom rates per chromosome
+o.hom.sum <- c()
+e.hom.sum <- c()
+for (i in 1:length(het.files)){
+  o.hom <- het.files[[i]]$`O.HOM.`
+  e.hom <- het.files[[i]]$`E.HOM.`
+  o.hom.sum <- cbind(o.hom.sum, o.hom)
+  e.hom.sum <- cbind(e.hom.sum, e.hom)
+}
+
+# bind matrices together, rename by chromosome
+het.rate <- cbind(o.hom.sum, e.hom.sum)
+colnames(het.rate)[1:22] <- paste0('O_HOM_', het.names)
+colnames(het.rate)[23:44] <- paste0('E_HOM_', het.names)
+#map.het.rate <- t(apply(het.rate, 1, function(z) mapply(function(x, y) (x -y)/x, z[1:22], z[23:44])))
+#het.rate <- cbind(het.rate, map.het.rate)
+
+# get total het rate for observed and expected, and find which indices are samples
+# that give het values more than 3 sd away from mean, either high or low
+o.hom.full <- rowSums(het.rate[, 1:22])
+e.hom.full <- rowSums(het.rate[, 23:44])
+het.rate.full <- (o.hom.full - e.hom.full) / o.hom.full
+het.rate.high <- which(het.rate.full > (mean(het.rate.full) + (3*sd(het.rate.full))))
+het.rate.low <- which(het.rate.full < (mean(het.rate.full) - (3*sd(het.rate.full))))
+# write FID and IID df to remove from plink dataset
+het.outliers.df <- het.files$chr1_het.het[c(het.rate.high, het.rate.low), c('X.FID', 'IID')]
+
+#########################################################################################
 
 # large list of all snp alt counts 
 # acount.files <- lapply(X = list.files(pattern = '*.acount', 
@@ -36,6 +63,9 @@ het.fail.tbl <- sort(table(unlist(het.fail)), decreasing = T)
 #                        comment.char = '', 
 #                        header = T)
 
+#######################################################################################
+
+# n. of missing genotypes per sample per chr
 smiss.files <- lapply(X = list.files(pattern = '*.smiss', 
                                      path = 'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/', 
                                      full.names = T), 
@@ -49,6 +79,33 @@ smiss.names <- lapply(X = list.files(pattern = '*.smiss',
                     function(x) gsub('^.*snp_filt_', '', x))
 
 names(smiss.files ) <- smiss.names
+
+# merge all chr files by IID
 smiss.full <- Reduce(function(x, y) merge(x, y, by = 'IID', all = T), smiss.files)
-sample.miss.rate <- rowMeans(smiss.full[, grepl('F_MISS', names(smiss.full))])
-sample.miss.fail <- smiss.full[which(sample.miss.rate > 0.1), 'IID']
+# subset smiss by only missing counts
+sample.miss.sum <- smiss.full[, grepl('MISSING_CT', names(smiss.full))]
+# get one number of all snps in analysis in any one sample
+sample.all.snps.count <- rowSums(smiss.full[1, grepl('OBS_CT', names(smiss.full))])
+# calculate the number of genotypes missing vs all snps number, per sample
+sample.miss.rate <- rowSums(sample.miss.sum)/sample.all.snps.count
+sample.miss.fail <- smiss.full[which(sample.miss.rate > 0.05), ]
+# write df with FID and IID of samples with > 5% genotypes missing
+sample.miss.df <- sample.miss.fail[, c('X.FID.x', 'IID')]
+names(sample.miss.df) <- c('X.FID', 'IID')
+
+# merged df of either het or missingness fails
+het.or.smiss.outliers.df <- unique.data.frame(rbind(het.outliers.df, sample.miss.df))
+
+# now need to remove withdrawn participants
+withdrawn.consent <- read.csv('C:/Users/Prasanth/Documents/cardiac_gwas/participants_withdrawn.csv', header = F)
+withdrawn.consent.samples <- het.files$chr1_het.het[het.files$chr1_het.het$IID %in% withdrawn.consent$V1, 'IID']
+withdrawn.consent.samples.df <- data.frame('X.FID' = withdrawn.consent.samples, 
+                                           'IID' = withdrawn.consent.samples)
+
+het.smiss.withdrawn.df <- unique(data.frame(rbind(het.or.smiss.outliers.df, withdrawn.consent.samples.df)))
+
+# final outliers write out file
+# write.table(het.outliers.df, 'het_outlier_samples.txt', row.names = F, col.names = F, sep = '\t', quote = F)
+# write.table(sample.miss.df, 'excess_missing_samples.txt', row.names = F, col.names = F, sep = '\t', quote = F)
+# write.table(withdrawn.consent.samples.df, 'participant_withdrawn_samples.txt', row.names = F, col.names = F, sep = '\t', quote = F)
+#nrow write.table(het.smiss.withdrawn.df, 'het_smiss_withdrawn.txt', row.names = F, col.names = F, sep = '\t', quote = F)
