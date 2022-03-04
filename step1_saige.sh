01a0713cb20ec9c53a5b46f683ff9a605b13c99b
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-03-04 17:15:50 +0000

    remove array-based analysis

--- step1_saige.sh
+++ step1_saige.sh
@@ -2,13 +2,12 @@
 
 #SBATCH --partition=brc,shared
 #SBATCH --job-name=saige_1
-#SBATCH --time=04:00:00
-#SBATCH --mem=24G
+#SBATCH --time=08:00:00
+#SBATCH --mem=40G
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=8
 #SBATCH --verbose
-#SBATCH --output=/scratch/users/k2142172/tests/array/saige_step1_%A_%a.out
-#SBATCH --array=[1-22]%6
+#SBATCH --output=/scratch/users/k2142172/tests/array/saige_step1.out
 
 # load plink2
 module load apps/plink2/2.0.0a2
@@ -21,24 +20,22 @@ env=${base_dir}/packages/anaconda3/envs/RSAIGE
 # set variable paths for tools
 saige=${base_dir}/packages/SAIGE
 
-echo $SLURM_ARRAY_TASK_ID
-i=$SLURM_ARRAY_TASK_ID
-ls -alht ${out_dir}/pruned_chr${i}*
+ls -alht ${out_dir}/all_pruned_merged*
 
 # activate SAIGE environment
 conda activate $env
 
 
 Rscript ${saige}/step1_fitNULLGLMM.R \
-        --plinkFile="${out_dir}/pruned_chr${i}" \
-        --phenoFile=${out_dir}/chr${i}.pheno \
+        --plinkFile="${out_dir}/all_pruned_merged" \
+        --phenoFile=${out_dir}/pheno_file.pheno \
         --phenoCol=res_distensibility \
         --covarColList=PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,PC11,PC12,PC13,PC14,PC15,PC16,PC17,PC18,PC19,PC20 \
         --sampleIDColinphenoFile=IID \
         --traitType=quantitative \
         --invNormalize=TRUE \
-        --outputPrefix=${out_dir}/SAIGE_step1_chr${i} \
+        --outputPrefix=${out_dir}/SAIGE_step1 \
         --nThreads=2 \
         --IsOverwriteVarianceRatioFile=TRUE \
-        &> ${out_dir}/SAIGE_step1_chr${i}.out
+        &> ${out_dir}/SAIGE_step1.out
 
