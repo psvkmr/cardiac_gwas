13880b12f0ae215343d897a2fd7c1de1bcf299aa
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-03-04 17:09:47 +0000

    moved downstream analyses to subfolder

---
+++ analysis/analysis_plink_v_saige.R
@@ -0,0 +1,158 @@
+library(PCAtools)
+
+# plink v saige pvalues
+###############################################################################
+plink.merge <- plink.filt
+saige.merge <- saige.filt
+
+names(plink.merge)[c(1:5, 13, 14)] <- c('CHR', 'POS', 'SNPID', 'Allele1', 'Allele2', 'Tstat', 'p.value')
+
+merged <- full_join(plink.merge, 
+                    saige.merge, 
+                    by = c('CHR', 'POS', 'SNPID'), 
+                    suffix = c('.plink', '.saige'))
+
+plot(-log10(merged$p.value.plink), -log10(merged$p.value.saige))
+plot(-log10(merged$BETA.plink), -log10(merged$BETA.saige))
+plot(-log10(merged$Tstat.plink), -log10(merged$Tstat.saige))
+
+View(head(arrange(merged, p.value.plink)))
+
+
+###########################################################################
+# read pheno files
+
+pheno.file.paths <- list.files(path = 'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/pheno',
+                               pattern = 'chr.*.pheno', full.names = T)
+pheno.files <- lapply(pheno.file.paths, read.table, header = T)
+pheno.names <- gsub('.pheno', '', gsub('^.*/pheno/', '', pheno.file.paths))
+names(pheno.files) <- pheno.names
+
+# plot(pheno.files$chr7$PC1, pheno.files$chr7$res_distensibility)
+# plot(pheno.files$chr7$PC2, pheno.files$chr7$res_distensibility)
+ggplot(pheno.files$chr7, aes(PC1, PC2)) + 
+  geom_point(aes(colour = res_distensibility), alpha = 0.8) +
+  scale_colour_viridis_c()
+
+pcas <- lapply(pheno.files, function(x) as.matrix(x[, 3:22]))
+
+
+###############################################################################
+# extract sig snp genotypes
+
+extract.saige.dosages <- saige.sig[, c('CHR', 'POS')]
+# write.table(extract.saige.dosages, 'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/extract_saige_dosages.tab',
+#            quote = F, sep = '\t', col.names = F, row.names = F)
+# use bcftools on rosalind to extract from dosage vcf files
+
+saige.sig.dosages.chr7 <- read.table('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/extracted_saige_sig_dosages_chr7.tab', 
+                                     comment.char = '', header = F)
+vcf.samples <- read.table('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/vcf_sample_names.txt')
+vcf.headers <- c('CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', vcf.samples)
+names(saige.sig.dosages.chr7) <- vcf.headers
+
+saige.sig.dos.7.mat <- dplyr::select(saige.sig.dosages.chr7, -c('CHROM', 'POS', 'REF', 'ALT', 'QUAL')) %>% 
+  column_to_rownames('ID')
+
+rownames(saige.sig.dos.7.mat) <- gsub('^X.*_', '', rownames(saige.sig.dos.7.mat))
+saige.sig.dos.7.mat <- saige.sig.dos.7.mat %>% as.data.frame() %>% rownames_to_column('ID')
+
+########################################################################################
+# check genotype to dist, pc1 correlations
+
+pheno.7 <- pheno.files$chr7
+pheno.7$FID <- as.character(pheno.7$FID)
+saige.sig.dos.res.7 <- left_join(pheno.7, saige.sig.dos.7.mat, by = c('FID' = 'ID'))
+saige.sig.dos.res.7 <- na.omit(saige.sig.dos.res.7)
+
+# cor(saige.sig.dos.res.7$PC1, saige.sig.dos.res.7$rs35361607)
+# cor(saige.sig.dos.res.7$PC2, saige.sig.dos.res.7$rs35361607)
+# cor(saige.sig.dos.res.7$res_distensibility, saige.sig.dos.res.7$rs35361607)
+# cor(saige.sig.dos.res.7$PC1, saige.sig.dos.res.7$res_distensibility)
+
+saige.sig.dos.res.7$dos.total <- rowSums(saige.sig.dos.res.7[, c(14:ncol(saige.sig.dos.res.7))])
+
+cor(saige.sig.dos.res.7$PC1, saige.sig.dos.res.7$dos.total)
+cor(saige.sig.dos.res.7$PC2, saige.sig.dos.res.7$dos.total)
+cor(saige.sig.dos.res.7$res_distensibility, saige.sig.dos.res.7$dos.total)
+cor(saige.sig.dos.res.7$PC1, saige.sig.dos.res.7$res_distensibility)
+cor(saige.sig.dos.res.7$PC2, saige.sig.dos.res.7$res_distensibility)
+
+plot(saige.sig.dos.res.7$res_distensibility, saige.sig.dos.res.7$dos.total)
+
+######################################################################
+# pca outliers
+
+pca.files <- lapply(list.files(path = 'C:/Users/Prasanth/Documents/cardiac_gwas/gwas_results/pca',
+                          pattern = 'pca_chr.*.eigenvec', full.names = T),
+               read.table, header = F)
+pca.filenames <- list.files(path = 'C:/Users/Prasanth/Documents/cardiac_gwas/gwas_results/pca',
+                            pattern = 'pca_chr.*.eigenvec', full.names = T)
+pca.filenames <- gsub('.eigenvec', '', gsub('^.*/pca/pca_', '', pca.filenames))
+names(pca.files) <- pca.filenames
+
+detectOutliers <- function(x) {
+  mean.x <- mean(x)
+  sd.x <- sd(x)
+  outliers <- c()
+  for (i in 1:length(x)){
+    if (x[i] > mean.x + 3*sd.x) {
+      outliers <- c(outliers, x[i])
+    } else if (x[i] < mean.x - 3*sd.x) {
+      outliers <- c(outliers, x[i])
+    }
+  }
+  outliers.rows <- match(outliers, x)
+  return(outliers.rows)
+}
+
+pca.filter.columns <- c(3:4)    
+pc.outliers <- lapply(pca.files, function(x) x[, pca.filter.columns])
+pc.outliers <- lapply(pc.outliers, function(x) lapply(x, detectOutliers))
+pca.outlier.samples <- lapply(pc.outliers, function(x) unique(unlist(x)))
+pca.all.outlier.samples <- unique(unlist(pca.outlier.samples))
+pca.outlier.samples.df <- data.frame(fid = pca.all.outlier.samples, iid = pca.all.outlier.samples)
+final.samples <- lapply(pca.files, `[[`, 1)
+final.samples <- Reduce(intersect, final.samples)
+final.samples <- final.samples[-pca.all.outlier.samples]
+final.samples.df <- data.frame(fid = final.samples, iid = final.samples)
+# write.table(final.samples.df, 
+#             "C:/Users/Prasanth/Documents/cardiac_gwas/gwas_results/final_samples.txt", 
+#             col.names = F, row.names = F, quote = F, sep = '\t')
+final.pcas <- lapply(pca.files, function(x) x[x$V1 %in% final.samples, ])
+final.pcas <- lapply(final.pcas, function(x) `names<-`(x, c('#FID', 'IID', paste0(rep('PC'), 1:10))))
+# for (i in 1:length(final.pcas)){
+#   write.table(final.pcas[[i]],
+#               file = paste0('C:/Users/Prasanth/Documents/cardiac_gwas/strict_gwas/pcas/pca_',
+#                             pca.filenames[i], '.eigenvec', sep = ''),
+#               row.names = F, quote = F, sep = '\t')
+# }
+
+
+###############################################################################
+
+#saige vep 
+
+saige.vep <- read.table('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige/vep_output_saige_sig.txt', 
+                        comment.char = '', header = T)
+
+saige.vep.sub <- saige.vep[, c('X.Uploaded_variation', 'Allele', 'Consequence', 
+                               'IMPACT', 'SYMBOL', 'Gene', 'BIOTYPE', 'AF', 'AFR_AF', 
+                               'AMR_AF', 'EAS_AF', 'EUR_AF', 'SAS_AF')]
+saige.vep.sub <- unique.data.frame(saige.vep.sub)
+
+saige.vep.sub <- merge(saige.sig, saige.vep.sub, by.x = 'SNPID', by.y = 'X.Uploaded_variation')
+af.comparison <- saige.vep.sub[, c('SNPID', 'Allele1', 'Allele2', 'AF_Allele2', 
+                                   'Allele', 'AF', 'AMR_AF', 'EUR_AF')]
+af.correlation.scatter <- plot(af.comparison$AF_Allele2, af.comparison$EUR_AF)
+#plot(af.comparison$AF_Allele2, af.comparison$AMR_AF)
+#table(saige.vep.sub$SYMBOL)
+
+#####################################################################################
+# mitchell
+mitchell.cat <- read_delim('mitchell_gwas_pubmedid_22068335.tsv', delim = '\t')
+mitchell.own <- read.csv('mitchell_own_table.csv')
+
+mitchell.own$p.value <- gsub('\\?', '-', mitchell.own$p.value)
+mitchell.own$p.value <- gsub(' x 10', 'e', mitchell.own$p.value)
+mitchell.own$p.value <- as.numeric(mitchell.own$p.value)
---
+++ analysis/analysis_saige_gwas.R
@@ -0,0 +1,82 @@
+library(tidyverse)
+
+#https://github.com/GP2-TNC-WG/GP2-Bioinformatics-course/blob/master/Module_III.md
+saige.dir <- 'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige/'
+saige.files <- lapply(list.files(pattern = 'SAIGE_step2_chr.*.txt', 
+                                 path = paste0(saige.dir, 'outputs'), 
+                                 full.names = T), 
+                      read.table, comment.char = "", header = T)
+saige.names <- lapply(saige.files, function(x) paste0('chr', x$V1[1], sep = ''))
+names(saige.files) <- saige.names
+
+saige <- Reduce(rbind, saige.files)
+# write.csv(saige,
+#           'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige_summary_statistics.csv',
+#           row.names = F, quote = F)
+
+saige.sig <- filter(saige, p.value < 5E-8)
+# write.csv(saige.sig,
+#           'C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige_sig_summary_statistics.csv',
+#           row.names = F, quote = F)
+
+saige.log10 <- saige %>%
+  mutate(log10Praw = -1*log10(p.value)) %>%
+  mutate(log10P = ifelse(log10Praw > 40, 40, log10Praw)) %>%
+  mutate(log10Plevel = ifelse(p.value < 5E-08, "possible", NA)) %>%
+  mutate(log10Plevel = ifelse(p.value < 5E-09, "likely", log10Plevel))
+
+# Reduction of the GWAS object
+# This is for more efficient plotting
+# This drops everything not useful in future AUC calcs estiamte in Nalls et al., 2019
+
+saige.filt <- filter(saige.log10, log10P > 3.114074) %>%
+  arrange(CHR, POS)
+
+saige.plotting <- saige.log10 %>%
+  group_by(CHR) %>%
+  summarise(chr.len=max(POS)) %>%
+  mutate(tot = cumsum(as.numeric(chr.len)) - chr.len) %>%
+  dplyr::select(-chr.len) %>%
+  left_join(saige.filt, ., by = 'CHR') %>%
+#  left_join(saige.log10, ., by = 'CHR') %>%
+  arrange(ordered(CHR), POS) %>%
+  mutate(POScum=POS+tot)
+
+saige.axis.df <- saige.plotting %>%
+  group_by(CHR) %>%
+  summarise(center = (max(POScum) + min(POScum)) / 2)
+
+saige.manhattan <-
+  ggplot(saige.plotting, aes(POScum, log10P)) +
+  geom_point(aes(colour=as.factor(CHR)), alpha = 0.8, size = 0.8) +
+  scale_color_manual(values = rep(c("dark grey", "black"), 22)) +
+  scale_x_continuous(label = saige.axis.df$CHR, breaks = saige.axis.df$center) +
+  scale_y_continuous(expand = c(0, 0.05)) +
+  theme_classic() +
+  labs(x = "CHR", y = "-log10P") +
+  theme(legend.position = "none") +
+  geom_abline(intercept = -log10(5E-08), slope = 0, linetype = 2)
+# ggsave('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige_manhattan.png', 
+#        plot = saige.manhattan, dpi = 400)
+
+saige.qqplotter <- function(df) {
+  df <- df[!is.na(df$p.value), ]
+  df <- mutate(df, exp.pvalues = (rank(df$p.value, ties.method="first")+.5)/(length(df$p.value)+1))
+  ggplot(df, aes(-log10(exp.pvalues), -log10(p.value))) + 
+    geom_point() +
+    labs(x = "Expected logP", y = "Observed logP") + 
+    geom_abline(slope = 1, intercept = 0) +
+    #geom_label_repel(data = arrange(df, p.value)[1:5, ], box.padding = 0.5) +
+    theme_classic() 
+}
+
+saige.qqplt <- saige.qqplotter(saige)
+# ggsave('C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige_qqplot.png',
+#        plot = plink.qqplt, dpi = 400)
+
+saige.chisq <- qchisq(1-saige$p.value, 1)
+saige.lambda <- median(saige.chisq) / qchisq(0.5,1)
+#write.table(saige.lambda, "C:/Users/Prasanth/Documents/cardiac_gwas/clean_gwas/saige_lambda_value.txt")
+
+saige.alt.chisq <- qnorm(saige$p.value/2)
+saige.alt.lambda <- median(saige.alt.chisq^2, na.rm=T)/qchisq(0.5,df=1)
