1d1e7e21c92ac6b68ebda83c4e9bfb7b9a08af5a
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-03-04 17:14:50 +0000

    increase prune window, reduce mem requirement

--- prune_snps.sh
+++ prune_snps.sh
@@ -3,9 +3,7 @@
 #SBATCH --partition=brc,shared
 #SBATCH --job-name=gwas_prune
 #SBATCH --time=02:00:00
-#SBATCH --mem=24G
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
 #SBATCH --verbose
 #SBATCH --output=/scratch/users/k2142172/tests/array/gwas_prune_%A_%a.out
 #SBATCH --array=[1-22]%6
@@ -19,10 +17,11 @@ set -v
 #set -o noclobber
 
 #Load Plink module
+#Newer versions throw error about IDs
 module load apps/plink2/2.0.0a2
 
 out_dir=/scratch/users/k2142172/outputs/cardiac_gwas/gwas_run
-resources=/scratch/users/k2142172/resources/GRCh38
+resources=/scratch/users/k2142172/resources
 
 mkdir -p $out_dir
 
@@ -31,11 +30,11 @@ i=$SLURM_ARRAY_TASK_ID
 ls -alht ${out_dir}/sample_filt_chr${i}*
 
 
-plink2 --pfile ${out_dir}/sample_filt_chr${i} --memory 24000 --threads 2 \
- --indep-pairwise 50 5 0.2 --out ${out_dir}/LDprune_chr${i} 
+plink2 --pfile ${out_dir}/sample_filt_chr${i} --memory 16000 \
+ --indep-pairwise 500 50 0.2 --out ${out_dir}/LDprune_chr${i} 
 wait
-plink2 --pfile ${out_dir}/sample_filt_chr${i} --memory 24000 --threads 2 \
---exclude bed0 ${resources}/highLDRegionsIDs.txt \
+plink2 --pfile ${out_dir}/sample_filt_chr${i} --memory 16000  \
+--exclude bed0 ${resources}/highLDRegionsIDs_GRCh37.txt \
 --extract ${out_dir}/LDprune_chr${i}.prune.in --make-pgen --out ${out_dir}/pruned_chr${i}
 wait
-plink2 --pfile ${out_dir}/pruned_chr${i} --make-bed --out ${out_dir}/pruned_chr${i}
+plink2 --pfile ${out_dir}/pruned_chr${i} --memory 16000 --make-bed --out ${out_dir}/pruned_chr${i}
