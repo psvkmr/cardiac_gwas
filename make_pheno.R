581bb8f472cca0061d025b71c9b3675ef2da7e20
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-03-06 11:24:40 +0000

    missing comma

--- make_pheno.R
+++ make_pheno.R
@@ -5,7 +5,7 @@ args=commandArgs(trailingOnly=T)
 out_dir <- '/scratch/users/k2142172/outputs/cardiac_gwas/gwas_run/'
 
 f <- read.table(args[1], header=T, fill=T)
-p <- read.table(paste0(out_dir, 'pca.eigenvec', sep = '') header=F)
+p <- read.table(paste0(out_dir, 'pca.eigenvec', sep = ''), header=F)
 
 names(p) <- c('FID', 'IID', paste0(rep('PC'), seq(1, 20), sep = ''))
 df <- left_join(p, f, by = c('FID', 'IID'))
