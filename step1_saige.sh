ab9efe1703eb07976c93d16f140cd626647d432a
Prasanth Sivakumar <prasanth.sivakumar@kcl.ac.uk>
2022-03-06 11:26:10 +0000

    big time and mem requirement increase

--- step1_saige.sh
+++ step1_saige.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --partition=brc,shared
 #SBATCH --job-name=saige_1
-#SBATCH --time=08:00:00
-#SBATCH --mem=40G
-#SBATCH --ntasks=2
+#SBATCH --time=12:00:00
+#SBATCH --mem=72G
+#SBATCH --ntasks=4
 #SBATCH --cpus-per-task=8
 #SBATCH --verbose
 #SBATCH --output=/scratch/users/k2142172/tests/array/saige_step1.out
@@ -13,6 +13,7 @@
 module load apps/plink2/2.0.0a2
 
 # set variable paths for files
+phenotype_col=$1
 base_dir=/scratch/users/k2142172
 out_dir=${base_dir}/outputs/cardiac_gwas/gwas_run
 env=${base_dir}/packages/anaconda3/envs/RSAIGE
@@ -29,13 +30,13 @@ conda activate $env
 Rscript ${saige}/step1_fitNULLGLMM.R \
         --plinkFile="${out_dir}/all_pruned_merged" \
         --phenoFile=${out_dir}/pheno_file.pheno \
-        --phenoCol=res_distensibility \
+        --phenoCol=$phenotype_col \
         --covarColList=PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10,PC11,PC12,PC13,PC14,PC15,PC16,PC17,PC18,PC19,PC20 \
         --sampleIDColinphenoFile=IID \
         --traitType=quantitative \
         --invNormalize=TRUE \
         --outputPrefix=${out_dir}/SAIGE_step1 \
-        --nThreads=2 \
+        --nThreads=4 \
         --IsOverwriteVarianceRatioFile=TRUE \
         &> ${out_dir}/SAIGE_step1.out
 
